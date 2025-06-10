d935e91b680e4b11757775a708b1a437797a8a06
EssumanG <kojosayves7@gmail.com>
2025-06-10 13:24:36 +0000

    set up postgres db as RDS

---
+++ readme.md
@@ -0,0 +1,20 @@
+# ETL WITH AIRFLOW 
+An end-to-end data pipeline for analyzing user streaming behavior and generating business intelligence KPIs for a music streaming platform.
+-- 
+## Overview
+This project processes music streaming data from multiple sources to provide actionable insights through automated ETL pipelines and comprehensive analytics.
+
+### Architecture
+- **Data Sources**: Amazon RDS (user/song metadata), Amazon S3 (streaming events)
+- **Processing**: Extract, validate, transform streaming data and metadata
+- **Analytics**: Amazon Redshift for data warehousing and BI queries
+- **Development**: Docker containerized environment
+- **Deployment**: AWS cloud services
+
+
+
+## Getting Started
+
+### Prerequisites
+- *Docker (For Local Development)*
+- *AWS Account*
---
+++ scripts/sql/init-db.sql
@@ -0,0 +1,40 @@
+-- Create table for music tracks with audio features
+CREATE TABLE songs (
+    id INTEGER PRIMARY KEY,
+    track_id VARCHAR(50) NOT NULL UNIQUE,
+    artists TEXT,
+    album_name TEXT,
+    track_name TEXT,
+    popularity INTEGER ,
+    duration_ms INTEGER,
+    explicit BOOLEAN,
+    danceability DECIMAL(5,4),
+    energy DECIMAL(5,4),
+    key INTEGER,
+    loudness DECIMAL(8,3),
+    mode INTEGER,
+    speechiness DECIMAL(8,6),
+    acousticness DECIMAL(8,6),
+    instrumentalness DECIMAL(12,10),
+    liveness DECIMAL(5,4),
+    valence DECIMAL(5,4),
+    tempo DECIMAL(8,3),
+    time_signature INTEGER,
+    track_genre VARCHAR(50)
+);
+
+
+COPY songs FROM '/opt/sql-data/songs/songs.csv' WITH DELIMITER ',' CSV HEADER;
+
+
+-- Create users table
+CREATE TABLE users (
+    user_id INTEGER PRIMARY KEY,
+    user_name VARCHAR(100) NOT NULL,
+    user_age INTEGER,
+    user_country VARCHAR(100),
+    created_at DATE
+);
+
+
+COPY users FROM '/opt/sql-data/users/users.csv' WITH DELIMITER ',' CSV HEADER;
